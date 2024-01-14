6ad9c2759bbc656ec04c362f0f6e203348921b84
jasurumarov <jasurumarov199@gmail.com>
2024-01-14 19:37:48 +0500

    polar figma

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// HEADER BURGER MENU /////////////////////////
+
+ // JavaScript to toggle the mobile menu visibility
+ document.getElementById('toggleBtn').addEventListener('click', function () {
+    var mobileMenu = document.getElementById('mobileMenu');
+    mobileMenu.classList.toggle('hidden');
+});
+    
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#F2CEDA",
+        desc: "#150101",
+        btnBg: "#D23166",
+      },
+      fontFamily: {
+        roboto: "Roboto",
+        hind: "HindMadurai",
+        hind700: "HindMadurai700",
+        fredoka700: "Fredoka700",
+        hind600: "HindMadurai600",
+      }
+    },
+    screens: {
+      sm350: "350px",
+      sm: "480px",
+      md: "738px",
+      lg: "976px",
+      xl: "1164px",
+    }
+  },
+  plugins: [],
+}
+
